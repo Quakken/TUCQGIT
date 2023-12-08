aa5466555e16d4636155b15f5063a42e6f684ffc
Blazeclaw55 <Blazeclaw55@gmail.com>
2023-12-08 12:55:05 -0800

    Some stuff

---
+++ Assets/Scripts/MazeMinigameCode/DialogueOnCollide.cs
@@ -0,0 +1,26 @@
+/*
+ * Name: Alice Pocek
+ * Date: 12/7/2023
+ * Desc: Script that initiates text to appear when object is destroyed
+ */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueOnCollide : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //When player collides with said object initiate text
+        "You obtained /10 collectiables"
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
